55d1400571901eef901bfe0fed77b77484a2f452
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-05-24 11:05:49 +0200

    First commit

---
+++ desktop/project.clj
@@ -0,0 +1,20 @@
+(defproject checkers "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  
+  :dependencies [[com.badlogicgames.gdx/gdx "1.0.1"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.0.1"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.0.1"
+                  :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.0.1"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.0.1"
+                  :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-platform "1.0.1"
+                  :classifier "natives-desktop"]
+                 [org.clojure/clojure "1.6.0"]
+                 [play-clj "0.3.5"]]
+  
+  :source-paths ["src" "src-common"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :aot [checkers.core.desktop-launcher]
+  :main checkers.core.desktop-launcher)
---
+++ desktop/src/checkers/core/desktop_launcher.clj
@@ -0,0 +1,10 @@
+(ns checkers.core.desktop-launcher
+  (:require [checkers.core :refer :all])
+  (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
+           [org.lwjgl.input Keyboard])
+  (:gen-class))
+
+(defn -main
+  []
+  (LwjglApplication. checkers "checkers" 800 600)
+  (Keyboard/enableRepeatEvents true))
