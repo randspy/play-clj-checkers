bb66b8b6132f4623921fa7fd89f05a55bed954b2
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-05-29 10:26:55 +0200

    Basic mouse location added

--- desktop/src-common/checkers/board.clj
+++ desktop/src-common/checkers/board.clj
@@ -29,21 +29,24 @@
               [:wp 0 :wp 0 :wp 0 :wp 0]]))
 
 (defn- create-board-element[elem]
-  (cond
-     (= elem :w) "board/white.png"
-     (= elem :b) "board/black.png"
-     (= elem :bp) "board/black_pawn.png"
-     (= elem :wp) "board/white_pawn.png"
-     :else nil))
+  (case elem
+     :w "board/white.png"
+     :b "board/black.png"
+     :g "board/green.png"
+     :r "board/red.png"
+     :bp "board/black_pawn.png"
+     :wp "board/white_pawn.png"))
 
 (defn- generate-board-element [type x y]
-  (if (not (nil? type))
-    (assoc (texture type)
-      :x x
-      :y y
-      :width board-element-dimension-in-px
-      :height board-element-dimension-in-px)
-    []))
+  (let [board-element (create-board-element type)]
+    (if (not (nil? board-element))
+      (assoc (texture board-element)
+        :x x
+        :y y
+        :width board-element-dimension-in-px
+        :height board-element-dimension-in-px
+        :type type)
+      [])))
 
 (defn- calc-position-in-row-in-px [elem-number]
   (* board-element-dimension-in-px
@@ -65,7 +68,18 @@
         (recur (inc elem-index)
                (conj board-elements
                      (generate-board-element
-                      (create-board-element (nth flatten-board elem-index))
+                      (nth flatten-board elem-index)
                       (+ x-starting-position (calc-position-in-row-in-px elem-index))
                       (- y-starting-position (calc-position-in-col-in-px elem-index)))))))))
 
+
+
+(defn filter-location [entities position]
+  (let [x (:x position)
+        y (:y position)]
+    (filter #(and (< (:x %) x (+ (:x %) board-element-dimension-in-px))
+                  (< (:y %) y (+ (:y %) board-element-dimension-in-px)))
+            entities)))
+
+(defn filter-pawns [entities]
+  (filter #(= (:type %) :wp) entities))
--- desktop/src-common/checkers/core.clj
+++ desktop/src-common/checkers/core.clj
@@ -23,8 +23,15 @@
   (fn [screen entities]
     (cond
      (= (:key screen) (key-code :r)) (on-gl (set-screen! checkers main-screen))
-     :else entities)))
+     :else entities))
 
+  :on-touch-down
+  (fn [screen entities]
+    (let [pos (input->screen screen (:input-x screen) (:input-y screen))]
+      (do (print (board/filter-location (board/filter-pawns entities) pos))
+        entities))))
+
+(defn replace-board-elem [position old-elem new-elem])
 (defgame checkers
   :on-create
   (fn [this]
